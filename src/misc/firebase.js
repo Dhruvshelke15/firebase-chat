22d485ee7fafb0f0849ce199bd870a6fecfb10ca
Dhruv Shelke <dhruvshelke588@gmail.com>
2023-07-24 17:06:37 +0530

    initialized firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyBaNLb1Axa1hsFRIRy7jlCttXjRlwmVHus',
+  authDomain: 'fir-chat-812ce.firebaseapp.com',
+  projectId: 'fir-chat-812ce',
+  storageBucket: 'fir-chat-812ce.appspot.com',
+  messagingSenderId: '387257584392',
+  appId: '1:387257584392:web:5b1f27165a80786f6dfabd',
+};
+
+const app = firebase.initializeApp(config);
