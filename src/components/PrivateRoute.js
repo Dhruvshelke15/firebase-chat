ed193f764acc05aabb44a7306d8151d907a00ce1
Dhruv Shelke <dhruvshelke588@gmail.com>
2023-07-27 13:02:04 +0530

    manage user with profile context

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,10 +1,19 @@
 import { useContext } from 'react';
+import { Container, Loader } from 'rsuite';
 import { Route, Redirect } from 'react-router-dom';
 
 const PrivateRoute = ({ children, ...routeProps }) => {
-  const profile = useContext();
+  const { profile, isLoading } = useContext();
 
-  if (!profile) {
+  if (isLoading && !profile) {
+    return (
+      <Container>
+        <Loader center vertical size="md" content="Loading" speed="slow" />
+      </Container>
+    );
+  }
+
+  if (!profile && !isLoading) {
     return <Redirect to="/signin" />;
   }
   return <Route {...routeProps}>{children}</Route>;
--- src/components/PublicRoute.js
+++ src/components/PublicRoute.js
@@ -1,10 +1,20 @@
 import { Route, Redirect } from 'react-router-dom';
+import { Container, Loader } from 'rsuite';
+
 import { useProfile } from '../context/profile.context';
 
 const PublicRoute = ({ children, ...routeProps }) => {
-  const profile = useProfile();
+  const { profile, isLoading } = useProfile();
 
-  if (profile) {
+  if (isLoading && !profile) {
+    return (
+      <Container>
+        <Loader center vertical size="md" content="Loading" speed="slow" />
+      </Container>
+    );
+  }
+
+  if (profile && !isLoading) {
     return <Redirect to="/" />;
   }
   return <Route {...routeProps}>{children}</Route>;
