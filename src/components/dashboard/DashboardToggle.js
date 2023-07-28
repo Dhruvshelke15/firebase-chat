b6088d67ab313b52910c7bb303f835c3c6ffc1fb
Dhruv Shelke <dhruvshelke588@gmail.com>
2023-07-28 16:21:38 +0530

    fixed bugs with loading and added dashboard

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,11 +1,14 @@
-import { Button, Icon } from 'rsuite';
+import { Button, Drawer, Icon } from 'rsuite';
+import { useModalState } from '../../misc/custom-hooks';
 
 const DashboardToggle = () => {
+  const { isOpen, close, open } = useModalState();
   return (
     <>
-      <Button block color="blue">
+      <Button block color="blue" onClick={open}>
         <Icon icon="dashboard" /> Dashboard
       </Button>
+      <Drawer show={isOpen} onHide={close} placement="left"></Drawer>
     </>
   );
 };
