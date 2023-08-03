8335ec9783748f3a64e8da15a93da5386f305fb9
Dhruv Shelke <dhruvshelke588@gmail.com>
2023-08-03 17:28:04 +0530

    dsiplayed user data

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -14,7 +14,11 @@ const MessageItem = ({ message }) => {
           size="xs"
         />
 
-        <span className="ml-2">{author.name}</span>
+        <ProfileInfoBtnModal
+          profile={author}
+          appearance="link"
+          className="p-0 ml-1 text-black"
+        />
         <TimeAgo
           datetime={createdAt}
           className="font-normal text-black-45 ml-2"
