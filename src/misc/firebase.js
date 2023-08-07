b70328c23b579ecc2402751775b678c9e6d4b438
Dhruv Shelke <dhruvshelke588@gmail.com>
2023-08-07 13:52:36 +0530

    manage fcm from client

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,8 +1,11 @@
+import { Notification as Toast } from 'rsuite';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/storage';
 import 'firebase/messaging';
+import 'firebase/functions';
+import { isLocalhost } from './helpers';
 
 const config = {
   apiKey: 'AIzaSyD_I7IT3ev_EoYz1HXitfCIl-hxT04x4a4',
@@ -20,6 +23,7 @@ const app = firebase.initializeApp(config);
 export const auth = app.auth();
 export const database = app.database();
 export const storage = app.storage();
+export const functions = app.functions('asia-south1');
 export const messaging = firebase.messaging.isSupported()
   ? app.messaging()
   : null;
@@ -29,7 +33,12 @@ if (messaging) {
     'BB2Ly3Rf1EQarwryUiCt0PbSPtQaJOHn7K0UDh_XRhUgMKAJtjIuRKdBx--MlF9mJ6fxC_CARRAKncc-XHApnkE'
   );
 
-  messaging.onMessage(data => {
-    console.log(data);
+  messaging.onMessage(({ notification }) => {
+    const { title, body } = notification;
+    Toast.info({ title, description: body, duration: 0 });
   });
+}
+
+if (isLocalhost) {
+  functions.useFunctionsEmulator('http://localhost:5001');
 }
